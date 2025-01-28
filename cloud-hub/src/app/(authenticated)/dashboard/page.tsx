419c1af15c018c23d26584b4eaedbdef5f47c475
Christian <chrille_0313@hotmail.com>
2025-01-28 01:17:01 +0100

    chore: add skeleton for missing pages

---
+++ cloud-hub/src/app/(authenticated)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import { TitledSection } from '@/components/Sections';
+
+export default function Dashboard() {
+  return <TitledSection title="Dashboard" padding="2rem" gap="1rem"></TitledSection>;
+}
