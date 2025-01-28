95f64cd9a43d0158815c7cb71b14b239e7ab3b8d
Christian <chrille_0313@hotmail.com>
2025-01-28 01:17:01 +0100

    chore: add skeleton for missing pages

---
+++ cloud-hub/src/app/(authenticated)/shared/page.tsx
@@ -0,0 +1,5 @@
+import { TitledSection } from '@/components/Sections';
+
+export default function SharedClouds() {
+  return <TitledSection title="Shared Clouds" padding="2rem" gap="1rem"></TitledSection>;
+}
