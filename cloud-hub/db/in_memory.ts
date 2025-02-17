357c33793e0d23ceb9cbb30b0dd8e2fd7969daad
Christian <chrille_0313@hotmail.com>
2025-02-17 11:47:16 +0100

    fix: set user verified to false by default

--- cloud-hub/db/in_memory.ts
+++ cloud-hub/db/in_memory.ts
@@ -6,7 +6,7 @@ export class InMemoryDB {
     id: '3622c012-a12e-4c52-9863-4771fb2dbe5a',
     username: 'testUser',
     email: 'test@user1.com',
-    verified: true,
+    verified: false,
     createdAt: new Date(),
     updatedAt: new Date()
   };
