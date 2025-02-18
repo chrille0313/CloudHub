a5aff48ce21417647259b25a5e275cb78031ac2f
Mårten Granath <webdant2@gmail.com>
2025-02-18 23:46:56 +0100

    chore: added depenendencies for jest and axios

--- cloud-hub/__tests__/service.test.ts
+++ cloud-hub/__tests__/service.test.ts
@@ -1,62 +1,72 @@
-import { getAllClouds, createCloud } from '@/components/Cloud/service';
-import { Cloud, cloudSchema } from '@/components/Cloud/types';
+import { createCloud } from '@/components/Cloud/service';
+import { Cloud } from '@/components/Cloud/types';
 import { User } from '@/components/User/types';
-import { CloudOff, CloudSyncOutlined } from '@mui/icons-material';
-import mockInMemoryDB from '@/../db/__mocks__/in_memory';
-import exp from 'constants';
+import InMemoryDB from '../db/in_memory';
 
-/*
- *  Unit Tests
- */
+
+// ~~~~~~~~~~~~~~~~~~~~ //
+// ~    Constants     ~ //
+// ~~~~~~~~~~~~~~~~~~~~ //
 
 const testUser: User = {
-  id: '1',
+  id: '10',
   username: 'testUser',
-  email: 'test@test.test',
+  email: 'test@user.se',
   verified: true,
   createdAt: new Date(),
   updatedAt: new Date()
 };
 
 const testCloud: Cloud = {
-  id: '0',
+  id: '5',
   usedSize: 0,
   numberOfFiles: 0,
   createdAt: new Date(),
   updatedAt: new Date(),
   owner: testUser,
-  name: 'test',
+  name: 'testCloud',
   allocatedSize: 5
 };
 
+// ~~~~~~~~~~~~~~~~~~~~ //
+// ~    Unit Tests    ~ //
+// ~~~~~~~~~~~~~~~~~~~~ //
 
-jest.mock('@/../db/__mocks__/in_memory')
-  /*return {
-    
-    default: {
-      clouds: [] as Cloud[],
-      authenticatedUser: {
-        id: '3622c012-a12e-4c52-9863-4771fb2dbe5a',
-        username: 'testUser',
-        email: 'test@user1.com',
-        verified: false,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },  
-    },
-  };*/
-//createCloud()
-describe('create cloud', () => {
 
-  const cloudAttributes = {
-    name: 'test',
+/**
+ * ... for createCloud()
+ */
+
+describe('Unit tests for createCloud()', () => {
+
+  const cloudArgument = {
+    name: 'testCloud',
     owner: testUser,
     allocatedSize: 5
   };
 
-  test('should create cloud and match attributes', async () => {
+  test('Cloud attributes should match given arguments', async() => {
   
+    jest.spyOn(InMemoryDB, 'pushCloud').mockImplementation();
+    const createdCloud = await createCloud(cloudArgument);
+    
+    expect(createdCloud.name).toEqual(cloudArgument.name);
+    expect(createdCloud.owner).toBe(cloudArgument.owner);
+    expect(createdCloud.allocatedSize).toEqual(cloudArgument.allocatedSize);
+  });
+
+  test('Cloud attributes should be generated correctly', async() => {
+
+  })
+
+  /* test('should generate unique id for each cloud', async () => {
     const cloud1 = await createCloud(cloudAttributes);
+    const cloud2 = await createCloud(cloudAttributes);
+    expect(cloud1.id).not.toBe(cloud2.id);
+  }); */
+});
+
+/*
     expect(cloud1.id).toBeDefined();
     expect(cloud1.usedSize).toBe(0);
     expect(cloud1.numberOfFiles).toBe(0);
@@ -64,29 +74,16 @@ describe('create cloud', () => {
     expect(cloud1.updatedAt).toBeDefined;
     expect(cloud1.owner).toBe(cloudAttributes.owner);
     expect(cloud1.name).toBe(cloudAttributes.name);
-    expect(cloud1.allocatedSize).toBe(cloudAttributes.allocatedSize);
+    expect(cloud1.allocatedSize).toBe(cloudAttributes.allocatedSize); */
 
     //expect(mockInMemoryDB).toHaveBeenCalledTimes(1);
     
     //expect(inMemoryDB.mock.calls).toHaveLength(1)
     //expect(inMemoryDB.clouds).toHaveLength(1);
     //expect(inMemoryDB.clouds[0]).toBe(cloud1);
-  });
-
-  test('should generate unique id for each cloud', async () => {
-    const cloud1 = await createCloud(cloudAttributes);
-    const cloud2 = await createCloud(cloudAttributes);
-    expect(cloud1.id).not.toBe(cloud2.id);
-  });
-});
-
-
-
 
 
 //getAllClouds()
-
-
 //getSharedClouds()
 //getCloudById()
 //updateCloudById()
