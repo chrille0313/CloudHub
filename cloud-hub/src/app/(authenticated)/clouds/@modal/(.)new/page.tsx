01d2283480b08633a5a7789e68126aac19c00a3c
Christian <chrille_0313@hotmail.com>
2025-02-24 03:27:54 +0100

    feat: use titled section instead of title in modal

--- cloud-hub/src/app/(authenticated)/clouds/@modal/(.)new/page.tsx
+++ cloud-hub/src/app/(authenticated)/clouds/@modal/(.)new/page.tsx
@@ -1,10 +1,18 @@
 import CreateCloudForm from '@/ui/Forms/Clouds';
 import Modal from '@/ui/Modal';
+import { TitledSection } from '@/ui/Sections';
 
 export default function NewCloudModal() {
   return (
-    <Modal title="Create new cloud">
-      <CreateCloudForm />
+    <Modal>
+      <TitledSection
+        title="Create new cloud"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <CreateCloudForm />
+      </TitledSection>
     </Modal>
   );
 }
