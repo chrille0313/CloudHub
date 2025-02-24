091ed72e9d6057ae1734d9d3a8e35b143dfcaa1d
Christian <chrille_0313@hotmail.com>
2025-02-24 05:44:20 +0100

    refactor: refactor card grid to own component

--- cloud-hub/src/app/(authenticated)/clouds/page.tsx
+++ cloud-hub/src/app/(authenticated)/clouds/page.tsx
@@ -1,28 +1,20 @@
-import Grid from '@mui/material/Grid2';
 import { CloudCard } from '@/ui/Cards';
 import { TitledSection } from '@/ui/Sections';
 import { Cloud } from '@/components/Cloud/types';
 import CreateNewCloudCard from '@/ui/Cards/CloudCard/CreateNewCloudCard';
 import { getAllClouds } from '@/components/Cloud/service';
+import CardGrid from '@/ui/Containers/GridContainer';
 
 // TODO: Optimize using Suspense
 export default async function Clouds() {
   const clouds: Cloud[] = await getAllClouds();
-  const cloudCardSize = 3;
-  const cloudCardSizeSx = { xs: cloudCardSize * 4, sm: cloudCardSize * 2, md: cloudCardSize };
 
   return (
     <TitledSection title="My Clouds" padding="2rem" gap="1rem" flex={1}>
-      <Grid container spacing={{ xs: 2, md: 3 }} minHeight={200}>
-        <Grid size={cloudCardSizeSx}>
-          <CreateNewCloudCard />
-        </Grid>
-        {clouds.map((cloud) => (
-          <Grid key={cloud.name} size={cloudCardSizeSx}>
-            <CloudCard cloud={cloud} />
-          </Grid>
-        ))}
-      </Grid>
+      <CardGrid minHeight={200}>
+        <CreateNewCloudCard />
+        {...clouds.map((cloud) => <CloudCard key={cloud.name} cloud={cloud} />)}
+      </CardGrid>
     </TitledSection>
   );
 }
