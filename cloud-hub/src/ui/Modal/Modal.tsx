6844867b7e78b95aeb3883e3de0f9dc179808b28
Christian <chrille_0313@hotmail.com>
2025-02-24 03:29:10 +0100

    feat: make modal able to close

--- cloud-hub/src/ui/Modal/Modal.tsx
+++ cloud-hub/src/ui/Modal/Modal.tsx
@@ -1,22 +1,31 @@
 'use client';
 
-import { Dialog, DialogContent, DialogProps, DialogTitle } from '@mui/material';
+import { Dialog, DialogContent, DialogProps, DialogTitle, IconButton } from '@mui/material';
+import { useRouter } from 'next/navigation';
+import CloseIcon from '@mui/icons-material/Close';
 
 export interface ModalProps extends Omit<DialogProps, 'open'> {
   title?: string;
 }
 
 export default function Modal({ title, children, ...props }: ModalProps) {
-  // const router = useRouter();
+  const router = useRouter();
 
   function handleClose() {
-    // router.back();
+    router.back();
   }
 
   return (
     <Dialog open={true} onClose={handleClose} {...props}>
       {title && <DialogTitle>{title}</DialogTitle>}
       <DialogContent>{children}</DialogContent>
+      <IconButton
+        aria-label="close"
+        onClick={handleClose}
+        sx={{ position: 'absolute', top: '0.5rem', right: '0.5rem' }}
+      >
+        <CloseIcon />
+      </IconButton>
     </Dialog>
   );
 }
