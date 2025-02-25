28beddf34cef4dd4dfb159aae0d1f3ff2e623932
Christian <chrille_0313@hotmail.com>
2025-02-25 10:20:34 +0100

    feat: add title to sign up page form

--- cloud-hub/src/app/(unauthenticated)/(auth)/sign-up/page.tsx
+++ cloud-hub/src/app/(unauthenticated)/(auth)/sign-up/page.tsx
@@ -1,5 +1,17 @@
 import SignUpForm from '@/ui/Forms/Auth/SignUp';
+import { TitledSection } from '@/ui/Sections';
 
 export default function SignUpPage() {
-  return <SignUpForm />;
+  return (
+    <TitledSection
+      title="Sign Up"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      width="100%"
+      minHeight="100vh"
+    >
+      <SignUpForm />
+    </TitledSection>
+  );
 }
