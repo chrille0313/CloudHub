19b4c05b7de07f235355dc92e68789f0cf46fe67
Christian <chrille_0313@hotmail.com>
2025-02-25 10:04:44 +0100

    refactor: clean up middleware

--- cloud-hub/src/middleware.tsx
+++ cloud-hub/src/middleware.tsx
@@ -1,23 +1,22 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getSessionCookie } from 'better-auth';
 
+const protectedRoutes = ['/clouds', '/dashboard', '/shared', '/sign-out'];
+const publicRoutes = ['/', '/sign-in', '/sign-up'];
+
 export async function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const isProtectedRoute = protectedRoutes.includes(path);
+  const isPublicRoute = publicRoutes.includes(path);
+
   const sessionCookie = getSessionCookie(request);
 
-  if (!sessionCookie) {
-    if (
-      !request.nextUrl.pathname.startsWith('/sign-in') &&
-      !request.nextUrl.pathname.startsWith('/sign-up')
-    ) {
-      return NextResponse.redirect(new URL('/sign-in', request.url));
-    }
-  } else {
-    if (
-      request.nextUrl.pathname.startsWith('/sign-in') ||
-      request.nextUrl.pathname.startsWith('/sign-up')
-    ) {
-      return NextResponse.redirect(new URL('/dashboard', request.url));
-    }
+  if (isProtectedRoute && !sessionCookie) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
+
+  if (isPublicRoute && sessionCookie && !path.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
   return NextResponse.next();
@@ -25,11 +24,13 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/clouds/:path*',
-    '/dashboard/:path*',
-    '/shared/:path*',
-    '/sign-out/:path*',
-    '/sign-in/:path*',
-    '/sign-up/:path*'
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'
   ]
 };
