b613ae79318a758e36b78efb0ab3e4bf3b2f4271
Hampooo <hampe.akerlund@gmail.com>
2025-03-09 20:42:24 +0100

    test: additional tests for frontend and api
    tests for getAllClouds and createCloudForm

--- cloud-hub/__tests__/api.test.ts
+++ cloud-hub/__tests__/api.test.ts
@@ -1,12 +1,11 @@
 import { Cloud } from '@/components/Cloud/types';
 import { User } from '@/components/User/types';
 import InMemoryDB from '@/../db/in_memory';
-import axios from 'axios';
-
+import axios, { AxiosResponse } from 'axios';
+import { getAllClouds } from '@/components/Cloud/service';
 // ~~~~~~~~~~~~~~~~~~~~ //
 // ~    Constants     ~ //
 // ~~~~~~~~~~~~~~~~~~~~ //
-
 
 const testUser: User = {
   id: '10',
@@ -28,36 +27,34 @@ const testCloud: Cloud = {
   allocatedSize: 5
 };
 
-const API_URL = "http://localhost:3000/api/v1/clouds";
-
+const API_URL = 'http://localhost:3000/api/v1/clouds';
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 // ~~~~~~~~~~~~~~~~~~~~ //
 // ~ End-To-End Tests ~ //
 // ~~~~~~~~~~~~~~~~~~~~ //
-
 
 /**
  * ... for POST request
  */
 
 describe('Integration test for POST request', () => {
-    
-    test('Cloud should be posted to database matching given arguments', async() => {
-  
-        await axios.post(API_URL, testCloud)
-        .then(response => {
-            expect(response.status).toEqual(201);
-            const createdCloud = response.data.data
-            expect(createdCloud.usedSize).toEqual(testCloud.usedSize);
-            expect(createdCloud.name).toEqual(testCloud.name);
-            expect(createdCloud.allocatedSize).toEqual(testCloud.allocatedSize);
-            //expect(createdCloud.owner).toBe(testCloud.owner);
-        })
-    })
-})
-
-
-
-
+  test('Cloud should be posted to database matching given arguments', async () => {
+    const mockedResponse: AxiosResponse = {
+      data: testCloud,
+      status: 201,
+      statusText: 'OK',
+      headers: {},
+      config: { headers: new axios.AxiosHeaders() }
+    };
+    mockedAxios.get.mockResolvedValue(mockedResponse);
+    expect(mockedAxios.post).not.toHaveBeenCalled();
+    const createdCloud = await axios.post(API_URL, testCloud);
+    expect(mockedAxios.post).toHaveBeenCalledWith(API_URL, testCloud);
+    expect(mockedResponse.data).toEqual(testCloud);
+    expect(mockedResponse.status).toEqual(201);
+  });
+});
 
 /* 
 test("End-to-end test", async () => {
@@ -69,3 +66,32 @@ test("End-to-end test", async () => {
     expect(res2.statusCode).toEqual(200);
     expect(res2.body.map((task : Task) => task.description)).toContain(desc);
 }); */
+describe('getAllClouds()', () => {
+  test('My clouds are properly received', async () => {
+    const clouds: Cloud[] = [
+      {
+        id: '1',
+        usedSize: 0,
+        numberOfFiles: 0,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        owner: InMemoryDB.authenticatedUser,
+        name: 'testCloud',
+        allocatedSize: 5
+      }
+    ];
+    InMemoryDB.pushCloud(clouds[0]);
+    const mockedResponse: AxiosResponse = {
+      data: clouds,
+      status: 200,
+      statusText: 'OK',
+      headers: {},
+      config: { headers: new axios.AxiosHeaders() }
+    };
+    mockedAxios.get.mockResolvedValue(mockedResponse);
+
+    const receivedClouds = await getAllClouds();
+    expect(receivedClouds).toEqual(clouds);
+    InMemoryDB.clouds = [];
+  });
+});
--- cloud-hub/__tests__/frontend.test.tsx
+++ cloud-hub/__tests__/frontend.test.tsx
@@ -1,11 +1,15 @@
-import '@testing-library/jest-dom'
-import  CloudCard  from "@/ui/Cards/CloudCard"
-import InMemoryDB from '@/../db/in_memory'
-import { User } from '@/components/User/types'
-import { render, screen } from '@testing-library/react'
-import { act } from "react-dom/test-utils"
-import { Cloud } from '@/components/Cloud/types'
-import { MenuItem } from "@mui/material"
+import '@testing-library/jest-dom';
+import CloudCard from '@/ui/Cards/CloudCard';
+import InMemoryDB from '@/../db/in_memory';
+import { User } from '@/components/User/types';
+import { render, screen } from '@testing-library/react';
+import { act } from 'react';
+import { Cloud } from '@/components/Cloud/types';
+import { MenuItem } from '@mui/material';
+import axios, { AxiosResponse } from 'axios';
+import { getAllClouds } from '@/components/Cloud/service';
+import CreateCloudForm from '@/ui/Forms/Clouds/CreateCloudForm';
+import mockRouter from 'next-router-mock';
 
 // ~~~~~~~~~~~~~~~~~~~~ //
 // ~    Constants     ~ //
@@ -31,19 +35,37 @@ const testCloud: Cloud = {
   allocatedSize: 5
 };
 
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 // ~~~~~~~~~~~~~~~~~~~~ //
 // ~  Frontend Tests  ~ //
 // ~~~~~~~~~~~~~~~~~~~~ //
 
-describe('CloudCard', () => {
-    test('has correct properties', () => {
-      act(()=> {
-        render(<CloudCard cloud={testCloud} />)
-        
-        const cloudProperties = screen.getByRole('Cloudcard')
+describe('CreateCloudForm', () => {
+  test('renders form fields correctly', () => {
+    render(<CreateCloudForm />);
+    const nameField = screen.getByLabelText('Name');
+    const allocatedSizeField = screen.getByLabelText('Allocated size');
+    const unitField = screen.getByLabelText('Unit');
+    const submitButton = screen.getByRole('button', { name: 'Create' });
 
-        expect(cloudProperties).toBeInTheDocument();
-      });
-    })
-  })
+    expect(nameField).toBeInTheDocument();
+    expect(allocatedSizeField).toBeInTheDocument();
+    expect(unitField).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
+});
+
+describe('CloudCard', () => {
+  test('has correct properties', () => {
+    render(<CloudCard cloud={testCloud} />);
+    // Check for the cloud name
+    const cloudName = screen.getByText(testCloud.name);
+    expect(cloudName).toBeInTheDocument();
+
+    // Check for the allocated size
+    // const cloudSize = screen.getByText(testCloud.allocatedSize.toString());
+    // expect(cloudSize).toBeInTheDocument();
+  });
+});
